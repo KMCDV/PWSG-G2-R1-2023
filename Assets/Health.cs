f400c8ab65911c0af628582e224e481dd341acaf
kmalins1 <krzysztof.malinski@cdv.pl>
2023-04-28 20:00:51 +0200

    basic so lecture

---
+++ Assets/Health.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Health Info", menuName = "Health System/New Health Info", order = 1)]
+public class Health : ScriptableObject
+{
+    public int health;
+}
