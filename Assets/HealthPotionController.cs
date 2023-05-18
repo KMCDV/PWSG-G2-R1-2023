b55cb968f6d105e59de3f2ec2e71c058846c2e33
Krzysztof Maliński <krzysztof.malinski@cdv.pl>
2023-05-18 16:30:25 +0200

    cinemachine

--- Assets/HealthPotionController.cs
+++ Assets/HealthPotionController.cs
@@ -9,5 +9,31 @@ public class HealthPotionController : MonoBehaviour
     private void Start()
     {
         Debug.Log(howMuchHealthToHeal.health);
+        howMuchHealthToHeal.health = 100;
+        Coroutine debugCoroutine = StartCoroutine(DebugCoroutine());
+        StartCoroutine(SetHealthToZero());
+        StopCoroutine(debugCoroutine);
     }
+
+    private IEnumerator SetHealthToZero()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            yield return new WaitForSecondsRealtime(4f);
+            howMuchHealthToHeal.health = 0;
+        }
+
+        while (true)
+        {
+            continue;
+        }
+    }
+
+    private IEnumerator DebugCoroutine()
+    {
+        yield return new WaitUntil(IsHealthZero);
+        Debug.Log("Po jednej sekundzie");
+    }
+
+    private bool IsHealthZero() => howMuchHealthToHeal.health == 0;
 }
